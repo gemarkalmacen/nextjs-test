4cfb18ee9ec20e49bac1e364cb0f0df47784fd04
Markos <gmrk08@gmail.com>
2025-04-03 15:41:25 +0800

    fix 1

--- stellatest/next.config.ts
+++ stellatest/next.config.ts
@@ -1,14 +1,8 @@
-import type { NextConfig } from "next";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true, // Ensure this is set
-    serverActions: true, // Enables API routes in App Router
-  },
   env: {
     NEXTAUTH_URL: "https://devfe.surigaocity.gov.ph",
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
