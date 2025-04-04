a74ff71bca50aaccdd587c6cb37acc56b0e38db0
Markos <gmrk08@gmail.com>
2025-04-04 14:14:50 +0800

    added alloweddevorigins

--- stellatest/next.config.ts
+++ stellatest/next.config.ts
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  allowedDevOrigins: ["https://devfe.surigaocity.gov.ph", "http://192.168.200.19"],
   env: {
     NEXTAUTH_URL: "https://devfe.surigaocity.gov.ph",
   },
