8072ea4497979e1cbdd3c145762ee9bc3ff6a624
Markos <gmrk08@gmail.com>
2025-04-04 14:28:47 +0800

    t

--- stellatest/next.config.js
+++ stellatest/next.config.js
@@ -1,43 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/_next/(.*)", // Apply CORS to Next.js assets
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*", // Change this to your allowed origins if needed
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,POST,PUT,DELETE",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "X-Requested-With, Content-Type, Accept, Authorization",
-          },
-        ],
-      },
-    ];
-  },
+  allowedDevOrigins: [
+    "https://devfe.surigaocity.gov.ph",
+    "http://192.168.200.19",
+    "http://192.168.200.19:3003",
+    "http://localhost:3003"
+  ],
   env: {
     NEXTAUTH_URL: "https://devfe.surigaocity.gov.ph",
   },
 };
 
-module.exports = nextConfig;
-
+// module.exports = nextConfig;  // <== Ensure you're using module.exports
 
 
 // module.exports = {
-//   allowedDevOrigins: 
+//   allowedDevOrigins:
 //   [
 //     "https://devfe.surigaocity.gov.ph",
 //     "http://192.168.200.19",
 //     "http://192.168.200.19:3003",
 //     "http://localhost:3003",
-//     'local-origin.dev', 
+//     'local-origin.dev',
 //     '*.local-origin.dev',
 //     '192.168.200.19',
 //     '192.168.200.19:3003'
