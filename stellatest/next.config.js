1210020aa11817133ce920ee07bb7fb9cbf36812
Markos <gmrk08@gmail.com>
2025-04-04 14:24:24 +0800

    t

--- stellatest/next.config.js
+++ stellatest/next.config.js
@@ -1,14 +1,27 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  allowedDevOrigins: [
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   allowedDevOrigins: [
+//     "https://devfe.surigaocity.gov.ph",
+//     "http://192.168.200.19",
+//     "http://192.168.200.19:3003",
+//     "http://localhost:3003"
+//   ],
+//   env: {
+//     NEXTAUTH_URL: "https://devfe.surigaocity.gov.ph",
+//   },
+// };
+
+// module.exports = nextConfig;  // <== Ensure you're using module.exports
+
+
+module.exports = {
+  allowedDevOrigins: 
+  [
     "https://devfe.surigaocity.gov.ph",
     "http://192.168.200.19",
     "http://192.168.200.19:3003",
-    "http://localhost:3003"
-  ],
-  env: {
-    NEXTAUTH_URL: "https://devfe.surigaocity.gov.ph",
-  },
-};
-
-module.exports = nextConfig;  // <== Ensure you're using module.exports
+    "http://localhost:3003",
+    'local-origin.dev', 
+    '*.local-origin.dev'
+  ]
+}
--- stellatest/next.config.ts
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  allowedDevOrigins: ["https://devfe.surigaocity.gov.ph", "http://192.168.200.19", "http://192.168.200.19:3003", "http://localhost:3003"],
-  env: {
-    NEXTAUTH_URL: "https://devfe.surigaocity.gov.ph",
-  },
-};
-
-export default nextConfig;
